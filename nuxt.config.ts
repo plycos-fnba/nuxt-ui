b3fddc0025a15b3d7e959f9d824813f31d67d2ff
plycos-fnba <peter.lycos@fnba.com>
2023-11-28 09:15:29 -0500

    [peter.lycos] update deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    // baseURL: "/lycosp.dev.io/",
+    baseURL: "/nuxt-ui/",
     buildAssetsDir: "assets",
   },
   ssr: false,
